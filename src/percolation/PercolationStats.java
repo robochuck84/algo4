7f98ec26890a40876abaca1fc219f2d6cfefbac7
Travis <trogers84@gmail.com>
2013-08-31 08:09:06 +0100

    fixed percolation

--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -12,44 +12,73 @@ public class PercolationStats {
 		// perform T independent computational experiments on an N-by-N grid
 		double[] experimentValues = new double[T];
 		int totalSquares = N * N;
-		
+
 		Random random = new Random();
-		
+
 		for (int i = 0; i < T; i++) {
 			Percolation perc = new Percolation(N);
 			int numOpen = 0;
 			int x, y;
-			while(!perc.percolates()) {
-				x = random.nextInt();
-				y = random.nextInt();
-				perc.open(x, y);
-				numOpen++;
+			while (!perc.percolates()) {
+				x = random.nextInt(N) + 1;
+				y = random.nextInt(N) + 1;
+				if (!perc.isOpen(x, y)) {
+					perc.open(x, y);
+					numOpen = numOpen + 1;
+				}
 			}
-			experimentValues[i] = numOpen / totalSquares;
+			experimentValues[i] = numOpen / (double) totalSquares;
 		}
+
+		double sum = 0.0;
+		for (double exp : experimentValues)
+			sum += exp;
+
+		mean = sum / (double) T;
+
+		for (double exp : experimentValues)
+			sum += Math.pow((exp - mean), 2);
+
+		stddev = Math.sqrt(sum / (double) (T - 1));
+
+		double calc1 = (1.96 * stddev) / Math.sqrt(T);
+		confidenceLo = mean - calc1;
+		confidenceHi = mean + calc1;
 	}
 
 	public double mean() {
 		// sample mean of percolation threshold
-		return 0.0;
+		return mean;
 	}
 
 	public double stddev() {
 		// sample standard deviation of percolation threshold
-		return 0.0;
+		return stddev;
 	}
 
 	public double confidenceLo() {
 		// returns lower bound of the 95% confidence interval
-		return 0.0;
+		return confidenceLo;
 	}
 
 	public double confidenceHi() {
 		// returns upper bound of the 95% confidence interval
-		return 0.0;
+		return confidenceHi;
 	}
 
 	public static void main(String[] args) {
-		// test client, described below
+		int N = Integer.parseInt(args[0]);
+		int T = Integer.parseInt(args[1]);
+
+		PercolationStats stats = new PercolationStats(N, T);
+
+		String mean = "mean                    = " + stats.mean();
+		String stddev = "stddev                  = " + stats.stddev();
+		String conf = "95% confidence interval = " + stats.confidenceLo()
+				+ ", " + stats.confidenceHi();
+
+		System.out.println(mean);
+		System.out.println(stddev);
+		System.out.println(conf);
 	}
 }
